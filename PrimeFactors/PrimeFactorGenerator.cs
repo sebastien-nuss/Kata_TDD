ca5c4d1bb26e37894831a7d3346d41538b086937
Sebastien NUSS <3293988+sebastien-nuss@users.noreply.github.com>
2021-09-30 18:50:35 +0200

    First prime factors kata completed

---
+++ PrimeFactors/PrimeFactorGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace PrimeFactors
+{
+    public class PrimeFactorGenerator
+    {
+        public List<int> Generate(int v)
+        {
+            List<int> primes = new List<int>();
+
+            for (int factor = 2; v > 1; factor++)
+            {
+                for (; v % factor == 0; v /= factor)
+                    primes.Add(factor);
+            }
+
+            return primes;
+        }
+    }
+}
---
+++ PrimeFactors/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeFactors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
